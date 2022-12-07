aa2b865c3b0d9e07d8fad86ea746e2a319237278
nchertolin <nchertolin@gmail.com>
2022-12-07 20:21:45 +0500

    use react-text-mask for phone number in form

--- src/api.js
+++ src/api.js
@@ -1,14 +1,5 @@
-const phoneMask = str => {
-  let cleaned = ('' + str).replace(/\D/g, '');
-  let match = cleaned.match(/^(\d{3})(\d{3})(\d{2})(\d{2})$/);
-  if (match) {
-    return '(' + match[1] + ') ' + match[2] + '-' + match[3] + '-' + match[4];
-  };
-  return str;
-};
-
 const sendData = (name, phone, OnSuccsess, OnError, onFinally) => {
-  fetch(`https://api.telegram.org/bot5692087584:AAF5Jt4Wardrkb57J-m-IaWSCvNl3aMh5C4/sendMessage?chat_id=-1001639677585&text=${name}, 7 ${phone}`)
+  fetch(`https://ai.telegram.org/bot5692087584:AAF5Jt4Wardrkb57J-m-IaWSCvNl3aMh5C4/sendMessage?chat_id=-1001639677585&text=${name}, 7 ${phone}`)
     .then((response) => {
       if (response.ok) {
         OnSuccsess();
@@ -18,4 +9,4 @@ const sendData = (name, phone, OnSuccsess, OnError, onFinally) => {
     .finally(onFinally);
 };
 
-export { phoneMask, sendData };
+export { sendData };
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,55 +1,81 @@
 import { React, useRef } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import styles from './Form.module.scss';
 import flag from '../../assets/icons/russia.svg';
-import { phoneMask, sendData } from '../../api.js';
+import { sendData } from '../../api.js';
+import MaskedInput from 'react-text-mask';
 
 export default function Form({ isActive, setActive }) {
-  const { register, formState: { errors, }, handleSubmit, reset } = useForm({ mode: 'onSubmit', reValidateMode: 'onSubmit' });
-  const submitButton = useRef(null)
-  const info = useRef(null);
-  const infoError = useRef(null);
+  const { register, formState: { errors, }, handleSubmit, reset, control } = useForm({ mode: 'onSubmit', reValidateMode: 'onSubmit' });
+  const submitButton = useRef()
+  const succsessMessage = useRef();
+  const errorMessage = useRef();
+
+  const showMessage = (element) => {
+    element.classList.remove(styles.hidden)
+    setTimeout(() => {
+      element.classList.add(styles.hidden)
+    }, 5000)
+  };
 
   const onSubmit = ({ name, phone }) => {
     submitButton.current.disabled = true;
-    sendData(name, phone, () => info.current.classList.remove(styles.hidden), () => infoError.current.classList.remove(styles.hidden),
+    sendData(name, phone, () => showMessage(succsessMessage.current), () => showMessage(errorMessage.current),
       () => submitButton.current.disabled = false);
     reset();
   };
 
   return (
     <div className={`${styles.modal} ${isActive ? styles.active : ''}`}>
+
       <button type='button' className={styles.close} onClick={() => {
         reset();
         setActive(false)
       }}></button>
+
       <div className={styles.content}>
         <h4>Записаться</h4>
         <form autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
+
           <div className={styles.name}>
             <input className={errors?.name ? styles.invalid : ''} {...register('name', {
               required: 'Обязательноe поле.',
               pattern: {
                 value: /[а-яА-яa-zA-Z]/,
-                message: "Имя не должно содержать цифр."
+                message: "Имя должно содержать только буквы."
               }
             })} placeholder='Имя' />
             {errors?.name && <p className={styles.error}>{errors?.name.message}</p>}
           </div>
+
           <div className={styles.phone}>
             <div className={`${styles.phoneWrapper} ${errors?.phone ? styles.invalid : ''} `}>
               <div className={styles.country}>
                 <img className={styles.flag} src={flag} alt="RU" />
                 <span>+7</span>
               </div>
-              <input type='tel' {...register('phone', {
-                required: 'Обязательноe поле.',
-                minLength: {
-                  value: 15,
-                  message: 'Номер должен содержать 11 цифр.'
-                },
-                onChange: (evt) => { evt.target.value = phoneMask(evt.target.value) }
-              })} placeholder='(900) 000-00-00' maxLength={15} />
+              <Controller
+                control={control}
+                name='phone'
+                rules={{
+                  required: 'Обязательноe поле.',
+                  minLength: {
+                    value: 15,
+                    message: 'Номер должен содержать 11 цифр.'
+                  }
+                }}
+                render={({ field }) => (
+                  <MaskedInput
+                    className={styles.tel}
+                    mask={['(', /[0-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, '-', /\d/, /\d/]}
+                    placeholder='(900) 000-00-00'
+                    maxLength={15}
+                    guide={false}
+                    onChange={evt => field.onChange(evt)}
+                    value={field.value}
+                  />
+                )}
+              />
             </div>
             {errors?.phone && <p className={styles.error}>{errors?.phone.message}</p>}
           </div>
@@ -58,16 +84,18 @@ export default function Form({ isActive, setActive }) {
         <p>Персональные данные не будут переданы третьим лицам.</p>
       </div>
 
-      <div ref={info} className={`${styles.info} ${styles.hidden}`} onClick={(evt) => evt.target.classList.add(styles.hidden)}>
+      <div ref={succsessMessage} className={`${styles.info} ${styles.hidden}`}>
         <div onClick={evt => evt.stopPropagation()}>
           <span>Скоро с вами свяжется менеджер.</span>
         </div>
       </div>
-      <div ref={infoError} className={`${styles.info} ${styles.hidden}`} onClick={(evt) => evt.target.classList.add(styles.hidden)}>
+
+      <div ref={errorMessage} className={`${styles.info} ${styles.hidden}`}>
         <div onClick={evt => evt.stopPropagation()}>
           <span>Ошибка отправки формы, попробуйте записатся позже или свяжитесь с нами по телефону</span>
         </div>
       </div>
+
     </div>
   )
 }
