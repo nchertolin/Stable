a3cf6578601996fcbfcf2ba83335f6d6c833105e
nchertolin <nchertolin@gmail.com>
2023-01-12 15:38:52 +0500

    refactoring menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,8 +1,5 @@
 import styles from './Menu.module.scss';
 import { NavLink } from 'react-router-dom';
-import home from '../../assets/icons/home.svg';
-import car from '../../assets/icons/programms.svg';
-import add from '../../assets/icons/other.svg';
 
 const setActiveLink = ({ isActive }) => isActive ? `${styles.link} + ${styles.active}` : styles.link;
 
@@ -11,15 +8,12 @@ export default function Menu({ isActive, setActive }) {
     <div className={`${styles.modal} ${isActive ? styles.active : ''}`} >
       <ul className={styles.nav}>
         <li>
-          {/* <img src={home} alt="" /> */}
           <NavLink id={styles.main} className={setActiveLink} to='/' onClick={() => setActive(false)}><span>Главная</span></NavLink>
         </li>
         <li>
-          {/* <img src={car} alt="" /> */}
           <NavLink id={styles.programms} className={setActiveLink} to='/programms' onClick={() => setActive(false)}>Программы</NavLink>
         </li>
         <li>
-          {/* <img src={add} alt="" /> */}
           <NavLink id={styles.other} className={setActiveLink} to='/other' onClick={() => setActive(false)}>Дополнительно</NavLink>
         </li>
         <li>
